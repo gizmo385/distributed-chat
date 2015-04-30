ce502ad2698fd4b9184cefc0d2827164c9b4fd8a
Christopher(gizmo385) <cachapline8@gmail.com>
2015-04-30 11:27:42 -0700

    Server now handles client disconnects
    If an IOException occurs during sending/receiving of messages to/from the client, the server will
    now disconnect the client and prevent the server from puking everywhere.

--- src/Server.java
+++ src/Server.java
@@ -35,10 +35,11 @@ public class Server {
         } catch( IOException ioe ) {
             System.err.printf("Error while attempting to open server on port %d\n", portNumber);
             ioe.printStackTrace();
+            System.exit(1);
         }
 
         // Create the global chat room that all users can join
-        Room globalRoom = new Room();
+        Room globalRoom = new Room("Global Room");
         GLOBAL_ROOM_ID = globalRoom.getId();
         this.rooms.put(GLOBAL_ROOM_ID, globalRoom);
     }
@@ -69,6 +70,18 @@ public class Server {
         this.rooms.get(GLOBAL_ROOM_ID).addUser(userId);
     }
 
+    public <E extends Serializable> void sendMessageToRoom( Message<E> message, Room room ) {
+        if( room != null & message != null ) {
+            for( int userId : room.getUsers() ) {
+                ClientHandler ch = this.clientConnections.get(userId);
+
+                if( ch != null ) {
+                    ch.sendMessage(message);
+                }
+            }
+        }
+    }
+
     private class ClientHandler extends Thread {
         // Client information
         public final int userId;
@@ -95,12 +108,26 @@ public class Server {
             userIdCounter++;
         }
 
+        public void disconnect(boolean sendMessage) {
+            for( Room room : rooms.values() ) {
+                room.removeUser(this.userId);
+
+                if( sendMessage ) {
+                    // Notify all rooms that the user was in that this user has disconnected
+                    Message<String> disconnected = new Message<>("Server", room.getId(),
+                            String.format("%s has disconnected from %s", this.clientName,
+                                room.getName()), MessageType.CHAT);
+
+                    sendMessageToRoom(disconnected, room);
+                }
+            }
+        }
+
         public <E extends Serializable> void sendMessage(Message<E> messageToSend) {
             try {
                 this.writeToClient.writeObject(messageToSend);
             } catch( IOException ioe ) {
-                System.err.printf("Error writing to client!\n");
-                ioe.printStackTrace();
+                disconnect(false);
             }
         }
 
@@ -137,13 +164,7 @@ public class Server {
                     String.format("%s has joined the server!", clientName), MessageType.CHAT);
 
             Room globalRoom = rooms.get(GLOBAL_ROOM_ID);
-            for( int userId : globalRoom.getUsers() ) {
-                ClientHandler handler = clientConnections.get(userId);
-
-                if( handler != null ) {
-                    handler.sendMessage(joinedMessage);
-                }
-            }
+            sendMessageToRoom(joinedMessage, globalRoom);
 
             // Block until we recieve a message
             while( true ) {
@@ -153,28 +174,17 @@ public class Server {
                     Room destination = rooms.get(messageRecieved.getDestination());
 
                     if( destination != null ) {
-                        /*
-                         * If the room the user is sending to exists, distribute the message to all
-                         * users who are currently in that room.
-                         */
-                        for( int userId : destination.getUsers() ) {
-                            ClientHandler client = clientConnections.get(userId);
-                            client.sendMessage(messageRecieved);
-                        }
+                        sendMessageToRoom(messageRecieved, destination);
                     } else {
-                        /*
-                         * If the user tries to send a message to non-existent room, send them an
-                         * error message in response
-                         */
-                        Message<String> errorMessage = new Message<String>("Server", -1,
+                        Message<String> errorMessage = new Message<>("Server", -1,
                                 String.format("%d is not a valid room id!",
                                     messageRecieved.getDestination()), MessageType.ERROR);
 
                         writeToClient.writeObject(errorMessage);
                     }
                 } catch( IOException ioe ) {
-                    System.err.printf("Error while reading message from client!\n");
-                    ioe.printStackTrace();
+                    disconnect(true);
+                    break;
                 } catch( ClassNotFoundException cnfe ) {
                     System.err.printf("Invalid message class recieved over socket!\n");
                     cnfe.printStackTrace();
