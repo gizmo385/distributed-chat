0d1696532350b5c075f8441c4283a75cee21244a
Christopher(gizmo385) <cachapline8@gmail.com>
2015-05-05 14:58:43 -0700

    Added client settings and a dialog to edit them

---
+++ src/ClientSettings.java
@@ -0,0 +1,111 @@
+import java.io.*;
+
+import java.awt.event.KeyEvent;
+
+public class ClientSettings implements Serializable {
+
+    private final static long serialVersionUID = 100L;
+
+    public transient static final ClientSettings DEFAULT = new ClientSettings("DEFAULT",
+            KeyEvent.VK_NUMPAD0, 50, 0, null, 0);
+
+    // Connection settings
+    private String hostname;
+    private int portNumber;
+
+    // Client settings
+    private String clientName;
+    private int touchToTalkKey;
+
+    // Bouncer settings
+    private int messageBouncerLimit;
+    private long lastLoginDate;
+
+    public ClientSettings(String clientName, int messageBouncerLimit, long lastLoginDate, int touchToTalkKey, String hostname, int portNumber ) {
+        this.clientName = clientName;
+        this.messageBouncerLimit = messageBouncerLimit;
+        this.lastLoginDate = lastLoginDate;
+        this.touchToTalkKey = touchToTalkKey;
+        this.hostname = hostname;
+        this.portNumber = portNumber;
+    }
+
+    public static void saveSettings(ClientSettings settings) {
+        File settingsFile = new File("settings.dat");
+
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(settingsFile))) {
+            settingsFile.createNewFile();
+            oos.writeObject(settings);
+        } catch( IOException ioe ) {
+            System.err.println("There was an error saving the settings!");
+            ioe.printStackTrace();
+        }
+
+    }
+
+    public static ClientSettings loadSettings() {
+        File settingsFile = new File("settings.dat");
+
+        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(settingsFile))) {
+            ClientSettings settings = (ClientSettings) ois.readObject();
+
+            return settings;
+        } catch( IOException ioe ) {
+            System.err.println("Error reading from settings file! Loading default settings!");
+        } catch( ClassNotFoundException cnfe ) {
+            System.err.println("settings.dat contains invalid data! Loading default settings!");
+        }
+
+        return ClientSettings.DEFAULT;
+    }
+
+    /* GETTERS */
+    public String getClientName() {
+        return this.clientName;
+    }
+
+    public int getTouchToTalkKey() {
+        return this.touchToTalkKey;
+    }
+
+    public int getMessageBouncerLimit() {
+        return this.messageBouncerLimit;
+    }
+
+    public long getLastLoginDate() {
+        return this.lastLoginDate;
+    }
+
+    public String getHostname() {
+        return this.hostname;
+    }
+
+    public int getPortNumber() {
+        return this.portNumber;
+    }
+
+    /* SETTERS */
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public void setTouchToTalkKey(int touchToTalkKey) {
+        this.touchToTalkKey = touchToTalkKey;
+    }
+
+    public void setMessageBouncerLimit(int messageBouncerLimit) {
+        this.messageBouncerLimit = messageBouncerLimit;
+    }
+
+    public void setLastLoginDate(long lastLoginDate) {
+        this.lastLoginDate = lastLoginDate;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public void setPortNumber(int portNumber) {
+        this.portNumber = portNumber;
+    }
+}
